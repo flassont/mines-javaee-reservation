bdf9f37917bff7428ea736da53c09194833744d6
nicolas <nicolas.hebrard@outlook.com>
2015-10-19 22:52:09 +0200

    Prémisse du moteur de template + fake servlet

--- src/main/java/org/emn/javaee/servlets/HelloServlet.java
+++ src/main/java/org/emn/javaee/servlets/HelloServlet.java
@@ -1,6 +1,8 @@
 package org.emn.javaee.servlets;
 
 import java.io.IOException;
+import java.util.Arrays;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,13 +12,30 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/")
+@WebServlet(urlPatterns = {"/login", "/signup", "/manager" })
 public class HelloServlet extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// response.getWriter().write(request.getContextPath());
+		if (request.getServletPath().equals("/login")) {
+			request.setAttribute("page", "template/connection/login.jspf");
+			request.setAttribute("title", "Connection");
+			request.getRequestDispatcher("/pages/main.jsp").forward(request, response);
+		}
+		if (request.getServletPath().equals("/signup")){
+			request.setAttribute("page", "template/connection/signup.jspf");
+			request.setAttribute("title", "Inscription");
+			request.getRequestDispatcher("/pages/main.jsp").forward(request, response);
+		}
+		if(request.getServletPath().equals("/manager")){
+			request.setAttribute("page", "template/manager.jsp");
+			request.setAttribute("title", "Manager");
+			request.setAttribute("types", Arrays.asList(new User("Type 1", 1), new User("Type 2", 2), new User("Type 3", 3)));
+			request.getRequestDispatcher("/pages/main.jsp").forward(request, response);
+		}
 	}
 
 }
---
+++ src/main/webapp/pages/template/manager.jsp
@@ -0,0 +1,3 @@
+<%-- <div><%@include file="navbar.jspf"%></div> --%>
+<div><%@include file="type/filter.jspf"%>
+	<%@include file="type/table.jspf"%></div>
