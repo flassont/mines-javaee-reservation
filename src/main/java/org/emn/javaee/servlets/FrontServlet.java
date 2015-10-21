e4b4fdfe98ca34c133375bdb6066de76049a6b55
naokha <xavier.huberdeau@gmail.com>
2015-10-21 16:34:29 +0200

    Suppression des actions

--- src/main/java/org/emn/javaee/servlets/FrontServlet.java
+++ src/main/java/org/emn/javaee/servlets/FrontServlet.java
@@ -14,8 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.emn.javaee.crud.UserCrud;
 import org.emn.javaee.models.User;
 
-import org.emn.javaee.actions.*;
-
 /**
  * Servlet implementation class HelloServlet
  */
@@ -23,69 +21,19 @@ import org.emn.javaee.actions.*;
 public class FrontServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-//	private ArrayList<Action> actions;
 
-//	public void init(ServletConfig config) throws ServletException {
-//		super.init(config);
-//		this.actions = new ArrayList<Action>();
-//		actions.add(new SignupAction());
-//	}
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String path = request.getPathInfo();
 		if(path.startsWith("/users")){
 			request.getRequestDispatcher("/users").forward(request, response);
 		}
-//		Action a = this.findAction(path);
-//		if(a == null)
-//		{
-//			System.out.println("action est null");
-//		}
-//		else 
-//		{
-//			a.handle(request);
-//			request.getRequestDispatcher("/pages/main.jsp").forward(request, response);
-//		}
-		// response.getWriter().write(request.getContextPath());
-		/*if (request.getServletPath().equals("/pages/login")) {
-			request.setAttribute("page", "template/connection/login.jspf");
-			request.setAttribute("title", "Connection");
-			request.getRequestDispatcher("/pages/main.jsp").forward(request, response);
-		}*/
-		/*if (request.getServletPath().equals("/pages/signup")){
-			request.setAttribute("page", "template/connection/signup.jspf");
-			request.setAttribute("title", "Inscription");
-			request.getRequestDispatcher("/pages/main.jsp").forward(request, response);
-		}*/
-		/*if(request.getServletPath().equals("/pages/manager")){
-			request.setAttribute("page", "template/manager.jsp");
-			request.setAttribute("title", "Manager");*/
-		//request.setAttribute("types", Arrays.asList(new User("Type 1", 1), new User("Type 2", 2), new User("Type 3", 3)));
-		/*request.getRequestDispatcher("/pages/main.jsp").forward(request, response);
-		}*/
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		this.doGet(request, response);
 	}
-	
-	/**
-	 * Find a action by its pattern
-	 * @param path
-	 * @return action if found else null
-	 */
-	/*private Action findAction(String path)
-	{
-		for(Action a: this.actions)
-		{
-			System.out.println(a.getPattern() + " vs " + path);
-			if(a.getPattern().equals(path))
-			{
-				System.out.println("test");
-				return a;
-			}
-		}
-		return null;
-	}*/
+
 }
