b983c3ea71aa02cc14cb0b13c419d9bb7ea72b6c
naokha <xavier.huberdeau@gmail.com>
2015-10-24 00:32:27 +0200

    Correction logout

--- src/main/java/org/emn/javaee/servlets/Logout.java
+++ src/main/java/org/emn/javaee/servlets/Logout.java
@@ -29,7 +29,7 @@ public class Logout extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		session.removeAttribute("user");
+		session.removeAttribute("authenticatedUser");
 		response.sendRedirect(getServletContext().getContextPath() + "/users");
 	}
 
