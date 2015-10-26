8c8066faa1b2b2c84df1d9fbfd5f047ab8a510ea
Nicolas <Nicolas@PC-Nicolas>
2015-10-26 23:37:19 +0100

    Javadoc + suppression de méthodes non utilisées.

--- src/main/java/org/emn/javaee/crud/ResourceTypeCrud.java
+++ src/main/java/org/emn/javaee/crud/ResourceTypeCrud.java
@@ -1,41 +1,27 @@
 package org.emn.javaee.crud;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.persistence.Query;
 
 import org.emn.javaee.models.ResourceType;
-import org.emn.javaee.tools.ValueParameter;
 
 /**
  * ResourceType CRUD
  */
-public class ResourceTypeCrud extends GenericCrud<ResourceType>{
+public class ResourceTypeCrud extends GenericCrud<ResourceType> {
 
-	public List<ResourceType> findBy(String name)
-	{
-		Map<String, Object> filters = new HashMap<>(1);
-		if(!name.isEmpty()) {
-			filters.put("name", new ValueParameter("%" + name + "%"));
-		}
-		return this.filter(filters);
-	}
-	
-	public boolean nameAlreadyExist(String name, int id)
-	{
+	public boolean nameAlreadyExist(String name, int id) {
 		String queryString = "SELECT * FROM RESOURCETYPE WHERE NAME = :name ";
-		if(id != -1)
-		{
+		if (id != -1) {
 			queryString += " AND ID != :id";
 		}
+		
 		Query query = this.em.createNativeQuery(queryString, ResourceType.class);
 		query.setParameter("name", name);
-		if(id != -1)
-		{
+		
+		if (id != -1) {
 			query.setParameter("id", id);
 		}
+		
 		return query.getResultList().size() != 0;
 	}
 
--- src/main/java/org/emn/javaee/crud/UserCrud.java
+++ src/main/java/org/emn/javaee/crud/UserCrud.java
@@ -1,45 +1,32 @@
 package org.emn.javaee.crud;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
 
 import org.emn.javaee.models.User;
-import org.emn.javaee.tools.ValueParameter;
 
 /**
  * User crud
  */
-public class UserCrud extends GenericCrud<User>{
+public class UserCrud extends GenericCrud<User> {
 
-	public List<User> findBy(String lastName, String firstName, boolean isAdmin)
-	{
-		Map<String, Object> filters = new HashMap<>(3);
-		if(!lastName.isEmpty()) {
-			filters.put("lastName", new ValueParameter("%" + lastName + "%"));
-		}
-		if(!firstName.isEmpty()) {
-			filters.put("firstName", new ValueParameter("%" + firstName + "%"));
-		}
-		filters.put("isAdmin", isAdmin);
-
-		return this.filter(filters);
-	}
-	
-	public User findByLoginAndPassword(String login, String password)
-	{
-		try
-		{
-			if(login == null && password == null) return null;
+	/**
+	 * Gets the user matching with the specified login and password.
+	 *  
+	 * @param login The login.
+	 * @param password The password.
+	 * @return The user or null if there is no match.
+	 */
+	public User findByLoginAndPassword(String login, String password) {
+		try {
+			if (login == null && password == null)
+				return null;
 			String queryString = "SELECT u from User u where u.login = :login and u.password = :password";
 			Query query = this.em.createQuery(queryString, User.class);
 			query.setParameter("login", login);
 			query.setParameter("password", password);
 			return (User) query.getSingleResult();
-		} catch(NoResultException e) {
+		} catch (NoResultException e) {
 			return null;
 		}
 	}
